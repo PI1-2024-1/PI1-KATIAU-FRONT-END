f97e973c279155a7094d3100601850b0067bab4c
pedroMADBR <pedrommartins.2014@gmail.com>
2024-06-19 13:25:32 -0300

    Textos na header comentados para posterior consulta com gerente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header() {
                 <Flex as="header" width="100%" padding="4" bg="#d81c1c" color="white" alignItems="center" justifyContent="space-around" height={'70px'}>
                     <Flex alignItems="center" >
                         <Heading as="h1" size="lg">KATIAU ⚡⚡ </Heading>
-                        <Heading as="h2" size="g"> ( Kart de Alta Tecnologia Integrada, Autônoma e Única ) </Heading>
+                        {/* <Heading as="h2" size="g"> ( Kart de Alta Tecnologia Integrada, Autônoma e Única ) </Heading> */}
                         {/* <p> Saiba mais em: </p>
                         <Link href='https://pi1-2024-1.github.io/PI1-KATIAU-docs/' > docs </Link> */}
                     </Flex>
